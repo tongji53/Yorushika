69f02b8f7c15425118cca7fb58604fb87f63eb20
tongji53 <tvxq-jyj.01180406@ezweb.ne.jp>
2020-10-04 15:20:41 +0900

    一覧画面と新規登録画面を実装

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -1,4 +1,22 @@
 class ArtsController < ApplicationController
   def index
+    @arts = Art.order(id: :asc)
+  end
+
+  def new
+    @art = Art.new
+  end
+
+  def create
+    @art = Art.new(art_params)
+    if @art.save
+      redirect_to arts_path
+    end
+  end
+
+  private
+
+  def art_params
+    params.require(:art).permit(:title, :content)
   end
 end
