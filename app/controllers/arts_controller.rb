5a2cfadd988a8c3c22266af57fd09553671ac01f
tongji53 <tvxq-jyj.01180406@ezweb.ne.jp>
2020-10-05 10:29:06 +0900

    コントローラを共通化

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -1,10 +1,11 @@
 class ArtsController < ApplicationController
+  before_action :set_art, only: %i[show edit update destroy]
+
   def index
     @arts = Art.order(id: :asc)
   end
 
   def show
-    @art = Art.find(params[:id])
   end
 
   def new
@@ -18,7 +19,24 @@ class ArtsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def updata
+    @art.update!(art_params)
+    redirect_to art
+  end
+
+  def destroy
+    @art.destroy
+    redirect_to root_path
+  end
+
   private
+
+  def set_art
+    @art = Art.find(params[:id])
+  end
 
   def art_params
     params.require(:art).permit(:title, :content)
